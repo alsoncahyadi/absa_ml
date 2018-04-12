fd93c064a3a8006fd1a3d81ab0f0cd9e4343309b
alsoncahyadi <alson.cahyadi@gmail.com>
2018-04-12 18:39:45 +0700

    fixed myonevsall

--- MyOneVsRestClassifier.py
+++ MyOneVsRestClassifier.py
@@ -49,11 +49,8 @@ def _predict_binary(estimator, X):
     """Make predictions using a single binary estimator."""
     if is_regressor(estimator):
         return estimator.predict(X)
-    try:
-        score = np.ravel(estimator.decision_function(X))
-    except (AttributeError, NotImplementedError):
         # probabilities of the positive class
-        score = estimator.predict_proba(X)[:, 1]
+    score = estimator.predict_proba(X)[:, 1]
     return score
 
 

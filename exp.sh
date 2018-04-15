ab89ed8a1dd9607d1d52c99e266b727d718df320
alsoncahyadi <alson.cahyadi@gmail.com>
2018-04-16 02:16:27 +0700

    added exp.sh

--- exp.sh
+++ exp.sh
@@ -1,3 +1,3 @@
-category_extraction/CnnCategoryExtractor.py > output/output_cnn.txt
-ote/OpinionTargetExtractor.py > output/output_lstm.txt
-sentiment_polarity/SentimentPolarityClassifier.py > output/output_cnn.txt
+category_extraction/CnnCategoryExtractor.py > category_extraction/output/output_cnn.txt
+ote/OpinionTargetExtractor.py > ote/output/output_lstm.txt
+sentiment_polarity/SentimentPolarityClassifier.py > sentiment_polarity/output/output_cnn.txt
